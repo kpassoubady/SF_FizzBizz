d058aebd02b3ae7636259b3683588ff49537bce1
kangs passoubady <kpassoubady@gmail.com>
2022-01-06 12:31:27 -0800

    added changeVersion method

--- Jenkins/writeMavenPom.groovy
+++ Jenkins/writeMavenPom.groovy
@@ -4,12 +4,22 @@ pipeline {
     stages {
         stage('Change POM Information') {
             steps {
-                pom = readMavenPom file: 'pom.xml'
-                pomVersionArray = pom.version.split('\\.')
-                pomVersionArray[0] = "${pomVersionArray[0]}".toInteger() + 1
-                pom.version = pomVersionArray.join('.')
-                writeMavenPom model: pom
+                echo "checkout code"
+                git 'https://github.com/kpassoubady/SF_FizzBizz.git'
+                echo "Current Maven Build version"
+                sh 'cat pom.xml'
+                changeVersion()
+                echo "After Changing Maven Build version"
+                sh 'cat pom.xml'
             }
         }
     }
+}
+
+private void changeVersion() {
+    pom = readMavenPom file: 'pom.xml'
+    pomVersionArray = pom.version.split('\\.')
+    pomVersionArray[0] = "${pomVersionArray[0]}".toInteger() + 1
+    pom.version = pomVersionArray.join('.')
+    writeMavenPom model: pom
 }
