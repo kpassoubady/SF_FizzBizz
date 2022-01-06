353c3f3edb386cdc1b0a116a717b82d4e0b1f13a
kangs passoubady <kpassoubady@gmail.com>
2022-01-06 11:36:15 -0800

    modified step def

--- Jenkins/writeMavenPom.groovy
+++ Jenkins/writeMavenPom.groovy
@@ -2,9 +2,9 @@ pipeline {
     agent any
 
     stages {
-        stage("Change POM Information") {
+        stage('Change POM Information') {
             steps {
-                def pom = readMavenPom file: 'pom.xml'
+                pom = readMavenPom file: 'pom.xml'
                 pomVersionArray = pom.version.split('\\.')
                 pomVersionArray[0] = "${pomVersionArray[0]}".toInteger() + 1
                 pom.version = pomVersionArray.join('.')
