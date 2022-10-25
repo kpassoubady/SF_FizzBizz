4a3580049dbecbccbdc48e9c62afe3c159778255
Kangeyan Passoubady <1095193+kpassoubady@users.noreply.github.com>
2022-10-25 08:03:27 -0700

    Added additional print info

--- Jenkins/readProperties.groovy
+++ Jenkins/readProperties.groovy
@@ -7,7 +7,7 @@ pipeline {
                 echo 'checkout code'
                 git 'https://github.com/kpassoubady/SF_FizzBizz.git'
                 echo 'Current server.properties contents'
-                sh 'cat ../src/test/resources/server.properties'
+                sh 'cat ./src/test/resources/server.properties'
                 readPropsInfo()
             }
         }
@@ -15,8 +15,8 @@ pipeline {
 }
 
 private void readPropsInfo() {
-    props = readProperties interpolate: true, file: '../src/test/resources/server.properties'
-    println props.url
-    println props.mode
-    println props.gc_browser_path
+    props = readProperties interpolate: true, file: './src/test/resources/server.properties'
+    println "URL==>" + props.url
+    println "Mode==>" + props.mode
+    println "GOOGLE CHROME BROWSER PATH: " + props.gc_browser_path
 }
